1f7486854cb294909cd68ff63d39138a9cb5fed4
ayhanakkaya <ayhanakkayameb@gmail.com>
2024-02-21 21:01:19 +0300

    ilk hafta bitti

---
+++ week01/full_stack_web_lesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace full_stack_web_lesson1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int yas=33;
+        string adSoyad="ayhan akkaya";
+        double boy=1.90;
+        char cinsiyet='E';
+        bool bekarMi=true;
+
+        Console.WriteLine(yas);
+        Console.WriteLine(adSoyad);
+        Console.WriteLine(boy);
+        Console.WriteLine(cinsiyet);
+        Console.WriteLine(bekarMi);
+    }
+}
---
+++ week01/full_stack_web_lesson2/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace full_stack_web_lesson2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /* byte skor1,skor2;
+        skor1=4;
+        skor2=4;
+
+        if(skor1>skor2){
+
+            Console.WriteLine("Skor1 Skor2 den büyüktür");
+        }
+        if(skor1<skor2){
+
+            Console.WriteLine("Skor1 Skor2 den küçüktür");
+
+        }
+        if(skor1==skor2){
+             Console.WriteLine("Skor1 Skor2 ye eşittir");
+        } */
+
+        //******************************************************
+
+       /*  Console.Write("Kullanıcı adını giriniz:");
+        string kullaniciAdi=Console.ReadLine();
+        if(kullaniciAdi=="ayhan"){
+            Console.WriteLine("Sisteme Hoşgeldin ayhan");
+        }
+        else{
+            Console.WriteLine("Yanlış kullanıcı");
+        } */
+
+        //********************************************************
+
+        Console.Write("Öğrencinin ortalamasını giriniz:");
+        int ortalama=Convert.ToInt16(Console.ReadLine());
+
+        if(ortalama<50){
+            Console.WriteLine("Sınıfta kaldı");
+        }
+
+        else if(ortalama<70){
+            Console.WriteLine("Geçti ama belge alamadı");
+        }
+
+        else if(ortalama<85){
+            Console.WriteLine("Teşekkür belgesi aldı");
+        }
+        else if(ortalama<=100){
+            Console.WriteLine("Takdir belgesi aldı");
+        }
+
+        else{
+            Console.WriteLine("0--100 aralığında bir sayı giriniz");
+        }
+
+
+    }
+}
